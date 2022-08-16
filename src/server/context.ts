8b99a2d1a712c96d3c7d68e8754f23caec24efec
Rey Mooy <gdrrey@gmail.com>
2022-08-16 13:56:00 +0800

    delete comments and log

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,16 +1,14 @@
 import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
 import { getSession } from 'next-auth/react';
 import prisma from '../lib/prisma'
-/**
- * Creates context for an incoming request
- * @link https://trpc.io/docs/context
- */
+
 export const createContext = async ({
   req,
   res,
-}) => {
+}: trpcNext.CreateNextContextOptions) => {
   const session = await getSession({ req });
-  console.log('createContext for', session?.user?.name ?? 'unknown user');
+
   return {
     req,
     res,
