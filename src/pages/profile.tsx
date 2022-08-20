16cf8d3bfa66f96f2018cc6a49e1e0a8ff8256d4
Rey Mooy <gdrrey@gmail.com>
2022-08-20 22:30:25 +0800

    add my order

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -9,6 +9,7 @@ import { trpc } from "../utils/trpc";
 export default function Profile() {
 
   const myProducts = trpc.useQuery(['product.get-my-products'])
+  const myOrders = trpc.useQuery(['order.get-my-orders'])
   
   const {data: session} = useSession()
 
@@ -21,7 +22,7 @@ export default function Profile() {
       {myProducts.isLoading ? <h1>Loading...</h1> : 
         <div className='p-6 grid grid-cols-1 gap-x-5 gap-y-3 justify-items-center sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
           {myProducts?.data?.length! < 1 && <h1>No products, create one</h1>}
-          {myProducts?.data?.map((product)=>(
+          {typeof myProducts !== 'string' && myProducts?.data?.map((product)=>(
             <Card 
             productName={product.name} 
             href={`/products/${product.id}`} 
@@ -35,6 +36,20 @@ export default function Profile() {
       {session && <Link href='/products/create'>
         <a className='block w-[200px] h-[40px] bg-yellow-400 p-1 rounded text-center'>Create Product</a>
       </Link>}
+
+      <h1>My Orders</h1>
+      <div>
+        {myOrders.isLoading ? <h1>Loading...</h1> : 
+          <div>
+            {myOrders?.data?.map(order=>(
+              <div key={order.id} className="w-[300px] h-[80px] p-3 m-1 rounded bg-white">
+                <p>Product Name : {order.product.name}</p>
+                <p>Seller : {order.orderTo.name}</p>
+              </div>
+            ))}
+          </div>
+        }
+      </div>
     </>
   );
 }
