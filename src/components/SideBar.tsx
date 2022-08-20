aecd92a077316754cb0916b789ec4cc3a432c4a1
Rey Mooy <gdrrey@gmail.com>
2022-08-20 22:29:54 +0800

    move signout to sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,12 +1,24 @@
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link'
 import React from 'react'
 
 export default function SideBar() {
+  const { status } = useSession();
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
   return (
-    <div className='h-screen w-[20%] fixed bg-slate-600 hidden lg:block'>
+    <div className='flex-col h-screen w-[20%] fixed bg-slate-600 hidden lg:flex'>
       <Link href='/'>
         <a>Home</a>
       </Link>
+      {status === "authenticated" && (
+        <>
+          <button onClick={handleSignOut}>SignOut</button>
+        </>
+      )}
     </div>
   )
 }
