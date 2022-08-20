48e6bf28cae39d287ab01d636bae3c198723d0ec
Rey Mooy <gdrrey@gmail.com>
2022-08-20 23:57:36 +0800

    mv singout & add loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import { useSession, signOut } from "next-auth/react";
-import { GetStaticProps } from 'next'
+import { useSession } from "next-auth/react";
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
 import Card from "../components/Card";
@@ -10,11 +9,6 @@ import { trpc } from "../utils/trpc";
 
 function Home() {
   const { status } = useSession();
-
-  const handleSignOut = async () => {
-    await signOut();
-  };
-
   const allProducts = trpc.useQuery(['product.get-products'])
 
   return (
@@ -32,23 +26,19 @@ function Home() {
             <>
               <Header/>
               <div>
-                <div className='p-6 grid grid-cols-1 gap-x-5 gap-y-3 justify-items-center sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
-                  {allProducts?.data?.map((r)=>(
-                    <Card 
-                    productName={r.name} 
-                    href={`/products/${r.id}`} 
-                    key={r.id} 
-                    userName={r?.user?.name} 
-                    userImage={r?.user?.image}/>
-                  ))}
-                </div>
+                {allProducts.isLoading ? <h1>Loading...</h1> : 
+                  <div className='p-6 grid grid-cols-1 gap-x-5 gap-y-3 justify-items-center sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+                    {allProducts?.data?.map((r)=>(
+                      <Card 
+                      productName={r.name} 
+                      href={`/products/${r.id}`} 
+                      key={r.id} 
+                      userName={r?.user?.name} 
+                      userImage={r?.user?.image}/>
+                    ))}
+                  </div>
+                }
               </div>
-
-              {status === "authenticated" && (
-                <>
-                  <button onClick={handleSignOut}>SignOut</button>
-                </>
-              )}
             </>
           )}
         </div>
