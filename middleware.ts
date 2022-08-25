b26cfcc9a6074e79ebd32a8c860136ce776b3cac
Rey Mooy <gdrrey@gmail.com>
2022-08-25 22:42:13 +0800

    del middleware

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @next/next/no-server-import-in-page
-import { NextRequest, NextResponse } from "next/server";
-
-export async function middleware(req: NextRequest){
-  console.log('its a middleware')
-  return NextResponse.next()
-}
