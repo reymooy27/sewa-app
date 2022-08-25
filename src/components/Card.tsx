6a11c82f32920733c7c002187ae85fdfdad491e5
Rey Mooy <gdrrey@gmail.com>
2022-08-25 20:43:38 +0800

    change user with shop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,24 +5,23 @@ import React from 'react'
 export type CardComponentType = {
   productName : string,
   href: string,
-  userName: string | null | undefined,
-  userImage: string | null | undefined
+  shopName: string
 }
 
-export default function Card({productName, href, userName, userImage}: CardComponentType) {
+export default function Card({productName, href, shopName}: CardComponentType) {
   return (
       <Link href={href}>
-        <a className='w-[150px] h-[250px] rounded p-[10px] flex flex-col grow
+        <a className='w-[150px] h-[250px] rounded p-[10px] flex flex-col shrink
         bg-white text-black hover:scale-[1.05] transition-all shadow-xl'>
           <div className='w-full h-[50%] bg-slate-500'>
             
           </div>
           <div>
             <h4>{productName}</h4>
-            <p>{userName}</p>
-            <div className='w-[20px]'>
+            <p>{shopName}</p>
+            {/* <div className='w-[20px]'>
               <Image src={userImage} height={20} width={20} alt={'Profile'}/>
-            </div>
+            </div> */}
           </div>
         </a>
       </Link>
