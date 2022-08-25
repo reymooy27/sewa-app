4a589236a1f22827fcea7c4d528fdd46b9816d8e
Rey Mooy <gdrrey@gmail.com>
2022-08-25 21:33:42 +0800

    add order props

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -33,7 +33,8 @@ export default function Profile() {
             {myOrders?.data?.map(order=>(
               <Order 
                 key={order.id}
-                productName = {order.product.name}
+                productName = {order?.product?.name}
+                seller={order?.shop?.name}
               />
             ))}
           </div>
@@ -54,14 +55,17 @@ Profile.getLayout = function getLayout(page: ReactElement){
 Profile.auth = true
 
 export type OrderComponentProps = {
-  productName: string
+  productName: string | null,
+  buyer?: string | null,
+  seller: string | null
 }
 
-export function Order({productName}: OrderComponentProps){
+export function Order({productName, buyer, seller}: OrderComponentProps){
   return (
     <div className="w-[300px] h-[80px] p-3 m-1 rounded bg-white text-black shadow-md">
       <p>Product Name : {productName}</p>
-      {/* <p>Seller : {orderTo}</p> */}
+      <p>Seller : {seller}</p>
+      <p>Buyer : {buyer}</p>
     </div>
   )
 }
