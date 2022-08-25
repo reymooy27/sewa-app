4774cf59d7b205f8ea7d52155bdc9714179af332
Rey Mooy <gdrrey@gmail.com>
2022-08-26 00:26:04 +0800

    fix type error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -31,7 +31,7 @@ export const authOptions: NextAuthOptions = {
     async session({session, token, user}){
       const shop = await prisma.shop.findUnique({
         where:{
-          userId: user?.id
+          userId: user?.id!
         }
       })
       if (session !== undefined) {
