e180ce60a99306eb883d4544aca6230d204c28de
Rey Mooy <gdrrey@gmail.com>
2022-08-25 23:00:09 +0800

    children type ReactNode

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import Header from './Header'
 
-export default function Layout({children}) {
+export default function Layout({children}: {children: ReactNode}) {
   return (
     <>
       <div className="flex flex-col w-full h-full">
