82d8eb100f0fd7abdf2e65600bd56e45b5611922
Rey Mooy <gdrrey@gmail.com>
2022-08-26 22:53:24 +0800

    fix prisma undefined in production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma =
+  global.prisma ||
+  new PrismaClient({})
 
-export default prisma;
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+export default prisma
